fefa20c7ce359b6b1f15a1ca94f4c7439b5a431a
Keith Woody <keith.woody@gmail.com>
2016-07-15 21:06:26 -0700

    Fix alias_method_chain DEPRECATION WARNING

--- lib/remotipart/render_overrides.rb
+++ lib/remotipart/render_overrides.rb
@@ -5,19 +5,19 @@ module Remotipart
     include ERB::Util
 
     def self.included(base)
-      base.class_eval do
-        alias_method_chain :render, :remotipart
-      end
+      base.prepend RenderWithRemotipart
     end
 
-    def render_with_remotipart *args
-      render_without_remotipart *args
-      if remotipart_submitted?
-        textarea_body = response.content_type == 'text/html' ? html_escape(response.body) : response.body
-        response.body = %{<script type=\"text/javascript\">try{window.parent.document;}catch(err){document.domain=document.domain;}</script>#{textarea_body}}
-        response.content_type = Mime::HTML
+    module RenderWithRemotipart
+      def render( *args )
+        super( *args )
+        if remotipart_submitted?
+          textarea_body = response.content_type == 'text/html' ? html_escape(response.body) : response.body
+          response.body = %{<script type=\"text/javascript\">try{window.parent.document;}catch(err){document.domain=document.domain;}</script>#{textarea_body}}
+          response.content_type = Mime::HTML
+        end
+        response_body
       end
-      response_body
     end
   end
 end
